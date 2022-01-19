b76dce21b8665596e6fa1c7bccb478b9713f333a
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-19 23:53:40 +0000

    added basic fetching url to file

--- fetcher.js
+++ fetcher.js
@@ -1,3 +1,17 @@
+const request = require('request');
+const fs = require('fs');
 const url = process.argv[2];
 const dest = process.argv[3];
-console.log(url, dest)
+
+const writeFile = (dest, content) => {
+  fs.writeFile(dest, content, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+  });
+}
+
+request(url, (error, response, body) => {
+  writeFile(dest, body);
+});
