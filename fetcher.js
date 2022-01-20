43da27d7b5458382ac7747d40e26037abf2e7ab6
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-20 00:33:07 +0000

    added check to see if going to overwrite file

--- fetcher.js
+++ fetcher.js
@@ -1,21 +1,43 @@
 const request = require('request');
+const readline = require('readline');
 const fs = require('fs');
 const url = process.argv[2];
 const dest = process.argv[3];
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const writeFile = (dest, content) => {
   fs.writeFile(dest, content, err => {
     if (err) {
       console.error(err)
-      return
+      return;
     }
+    process.exit(1)
   });
 }
+
+const checkIfFileExists = (dir, content) => {
+  if (fs.existsSync(dir)) {
+    // console.log('File exists! Do you want to overwrite?\n')
+    rl.question('File exists! Do you want to overwrite? [Y/N]\n', answer => {
+      if (answer !== 'n' && answer !== 'y') checkIfFileExists(dir, content);
+
+      if (answer === 'y') {
+        writeFile(dir, content);
+      } else {
+        console.log('Aborting!');
+        process.exit(1);
+      }
+    })
+  } else writeFile(dir, content);
+};
 
 request(url, (error, response, body) => {
   if (error !== null) {
     console.log(url,': Is not a valid URL');
     return;
   }
-  writeFile(dest, body);
+  checkIfFileExists(dest, body);
 });
